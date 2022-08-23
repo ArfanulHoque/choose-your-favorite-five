4841fc6261a6dc4fe188be3a5c362e99313c0b9b
ArfanulHoque <mderfan3928@gmail.com>
2022-08-23 20:12:34 +0600

    player expenses added

--- js/player.js
+++ js/player.js
@@ -1,9 +1,13 @@
+// list added
 const selectTable = [];
 
-
 function display(cardPlayer) {
+    if (cardPlayer.length > 5) {
+        alert('Already selected five')
+        return;
+    }
     const tableBody = document.getElementById('players-list');
-    tableBody.innerHTML = '';
+    tableBody.textContent = '';
 
     for (let i = 0; i < cardPlayer.length; i++) {
         const name = selectTable[i].playerName;
@@ -13,20 +17,28 @@ function display(cardPlayer) {
         <th>${i + 1}</th>
         <td>${name}</td>
         `;
-
-        if (cardPlayer.length > 5) {
-            alert('You are already selected 5 players')
-
-        }
-        else {
-            tableBody.appendChild(tr);
-
-        }
+        tableBody.appendChild(tr);
     }
+
+}
+function arrLentgh() {
+    const playeradd = selectTable.length;
+    if (playeradd > 5) {
+        return
+    }
+    return playeradd;
+}
+
+function getInputFieldByID(inputId) {
+    const inputFieldId = document.getElementById(inputId);
+    const inputFieldString = inputFieldId.value;
+    const inputField = parseFloat(inputFieldString);
+
+    inputFieldId.value = '';
+    return inputField;
 }
 
 function addToCart(element) {
-
 
     const playerName = element.parentNode.parentNode.children[0].innerText
     const playerSelect = {
@@ -35,16 +47,28 @@ function addToCart(element) {
 
     selectTable.push(playerSelect)
     display(selectTable);
+
+    arrLentgh()
 }
 
+// Player Expenses
 document.getElementById('btn-calculate').addEventListener('click', function () {
-    const perPlayerField = document.getElementById('per-player-field');
-    const newPerPlayerAmountString = perPlayerField.value;
-    const newPerPlayerAmount = parseFloat(newPerPlayerAmountString);
+    const inputField = getInputFieldByID('per-player-field')
+    const playeSelect = arrLentgh();
+    const totalPlayerExpenses = inputField * playeSelect;
+    const setValue = document.getElementById('player-expenses');
+    setValue.innerText = totalPlayerExpenses
+})
 
-    perPlayerField.value = '';
 
-
+// Manager and Coach Expenses
+document.getElementById('btn-total-calculate').addEventListener('click', function () {
+    const managerExpenses = getInputFieldByID('manager-field');
+    const coachExpenses = getInputFieldByID('coach-field');
+    const totalPlayerExpenses = document.getElementById('player-expenses');
 
 
 })
+
+
+
